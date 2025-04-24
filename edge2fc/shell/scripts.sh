8b4c5332e57398da11814e41997f7f0abfd2b943
abhishektandon <s22002@iitmadni.ac.in>
2025-04-24 19:17:29 +0530

    update

--- edge2fc/shell/scripts.sh
+++ edge2fc/shell/scripts.sh
@@ -5,12 +5,3 @@ python3 main.py --config configs/Template-BBDM.yaml --train --sample_at_start --
 #test
 # python3 main.py --config configs/Template-BBDM.yaml --sample_to_eval --gpu_ids 0 \
 # --resume_model path/to/model_ckpt --resume_optim path/to/optim_ckpt
-
-## LPIPS
-#python3 preprocess_and_evaluation.py -f LPIPS -s source/dir -t target/dir -n 1
-
-## diversity
-#python3 preprocess_and_evaluation.py -f diversity -s source/dir -n 1
-
-## fidelity
-#fidelity --gpu 0 --fid --input1 path1 --input2 path2
